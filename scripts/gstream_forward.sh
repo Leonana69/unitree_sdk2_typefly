d179459721be77870a192f1b59c753e882be4540
leonana69 <chengj0803@gmail.com>
2025-06-29 07:17:03 +0800

    update multicast address

--- scripts/gstream_forward.sh
+++ scripts/gstream_forward.sh
@@ -4,8 +4,13 @@ while ! pgrep -f '/unitree/module/video_hub/videohub'; do
     sleep 1
 done
 
+# gst-launch-1.0 -v \
+#   udpsrc address=230.1.1.1 port=1720 multicast-iface=eth0 \
+#   ! application/x-rtp, media=video, encoding-name=H264 \
+#   ! queue \
+#   ! udpsink host=230.1.1.2 port=1720 auto-multicast=true multicast-iface=wlan0
+
+
 gst-launch-1.0 -v \
   udpsrc address=230.1.1.1 port=1720 multicast-iface=eth0 \
-  ! application/x-rtp, media=video, encoding-name=H264 \
-  ! queue \
-  ! udpsink host=230.1.1.1 port=1720 auto-multicast=true multicast-iface=wlan0
+  ! udpsink host=230.1.1.2 port=1721 auto-multicast=true multicast-iface=wlan0
