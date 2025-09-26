a1decb55660df7538520cc14afdef7f4db3f21eb
leonana69 <chengj0803@gmail.com>
2025-09-27 02:40:07 +0800

    remove legged sport detect

--- scripts/control_interface.sh
+++ scripts/control_interface.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-while ! pgrep -f '/unitree/module/sport_mode/Legged_sport'; do
-    sleep 1
-done
-
 echo "Starting control interface..."
 /root/unitree_sdk2_typefly/build/bin/go2_control_interface &
