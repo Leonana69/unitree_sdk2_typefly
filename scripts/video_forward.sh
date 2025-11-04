1c6bd23a66fb522df76a123f6e5e0fd14eb291bf
leonana69 <chengj0803@gmail.com>
2025-11-05 04:22:51 +0800

    increase bps

--- scripts/video_forward.sh
+++ scripts/video_forward.sh
@@ -7,7 +7,7 @@ done
 gst-launch-1.0 v4l2src device=/dev/video2 ! \
     image/jpeg,width=2560,height=720,framerate=30/1 ! \
     jpegdec ! \
-    mpph264enc bps=300000 header-mode=1 ! \
+    mpph264enc bps=3000000 header-mode=1 ! \
     rtph264pay config-interval=-1 ! \
     udpsink host=230.1.1.1 port=1722 auto-multicast=true multicast-iface=wlan0 sync=false
 
