8bfc1c16c2c599aa276d9b57c04b7c07a83bf2ca
leonana69 <chengj0803@gmail.com>
2025-11-05 05:48:16 +0800

    add timestamp

--- scripts/dual_cameras.sh
+++ scripts/dual_cameras.sh
@@ -1,4 +1,4 @@
-gst-launch-1.0 v4l2src device=/dev/video2 ! \
+gst-launch-1.0 v4l2src device=/dev/video2 do-timestamp=true ! \
     image/jpeg,width=1600,height=600,framerate=20/1 ! \
-    rtpjpegpay quality=80 ! \
+    rtpjpegpay quality=80 timestamp-offset=0 ! \
     udpsink host=230.1.1.1 port=1722 auto-multicast=true multicast-iface=wlan0 sync=false
--- scripts/video_forward.sh
+++ scripts/video_forward.sh
@@ -4,9 +4,9 @@ while ! pgrep -f '/unitree/module/video_hub/videohub'; do
     sleep 1
 done
 
-gst-launch-1.0 v4l2src device=/dev/video2 ! \
+gst-launch-1.0 v4l2src device=/dev/video2 do-timestamp=true ! \
     image/jpeg,width=1600,height=600,framerate=20/1 ! \
-    rtpjpegpay quality=80 ! \
+    rtpjpegpay quality=80 timestamp-offset=0 ! \
     udpsink host=230.1.1.1 port=1722 auto-multicast=true multicast-iface=wlan0 sync=false
 
 # gst-launch-1.0 -v \
