d4a59aa414362082195dc71c4438821688dd92d4
leonana69 <chengj0803@gmail.com>
2025-11-15 07:30:05 +0800

    fix video forward

--- scripts/video_forward.sh
+++ scripts/video_forward.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Get directory of this script
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 ROBOT_ID=${ROBOT_ID:-1}
 MULTICAST_IP="230.1.1.${ROBOT_ID}"
 GSTREAMER_RGB_PORT=${GSTREAMER_RGB_PORT:-1722}
 GSTREAMER_DEPTH_PORT=${GSTREAMER_DEPTH_PORT:-1723}
 
-while ! pgrep -f '/unitree/module/video_hub/videohub'; do
-    sleep 1
-done
+# while ! pgrep -f '/unitree/module/video_hub/videohub'; do
+#     echo "Wait for video_hub to start..."
+#     sleep 1
+# done
 
 #### Dual fisheye camera forwarding
 # gst-launch-1.0 v4l2src device=/dev/video2 do-timestamp=true ! \
@@ -16,6 +20,7 @@ done
 #    udpsink host=${MULTICAST_IP} port=${GSTREAMER_RGB_PORT} auto-multicast=true multicast-iface=wlan0 sync=false
 
 #### Go2 native camera forwarding
+# echo "Forwarding Go2 camera stream to multicast IP ${MULTICAST_IP}:${GSTREAMER_RGB_PORT}"
 # gst-launch-1.0 -v \
 #   udpsrc address=230.1.1.1 port=1720 multicast-iface=eth0 \
 #   ! udpsink host=${MULTICAST_IP} port=${GSTREAMER_RGB_PORT} auto-multicast=true multicast-iface=wlan0
@@ -27,4 +32,4 @@ done
 #   ! udpsink host=${MULTICAST_IP} port=${GSTREAMER_RGB_PORT} auto-multicast=true multicast-iface=wlan0
 
 #### Depth Camera D435i Streaming
-python3 /root/unitree_sdk2_typefly/scripts/d435i.py
+python3 ${SCRIPT_DIR}/d435i.py
